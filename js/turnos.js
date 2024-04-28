b9552dd16fd3df98016dbb3ce5c0e3f47fdaa710
Monica <moniantich@gmail.com>
2024-04-28 18:55:24 -0300

    registro

--- js/turnos.js
+++ js/turnos.js
@@ -2,6 +2,7 @@ const menuPrincipal = document.getElementById("menuPrincipal");
 const vistaNuevoTurno = document.getElementById("vistaNuevoTurno");
 const vistaMisTurnos = document.getElementById("vistaMisTurnos");
 const divBotonVolver = document.getElementById("botonVolver");
+const vistaDatos_Personales = document.getElementById("registro_pacientes");
 
 const botonNuevoTurno = document.getElementById("nuevoTurno");
 botonNuevoTurno.addEventListener("click", () => {
@@ -17,10 +18,21 @@ botonMisTurnos.addEventListener("click", () => {
     divBotonVolver.style.display = "flex";
 })
 
+const botonRegistro = document.getElementById("datos_personales_boton");
+botonRegistro.addEventListener("click", () => {
+    menuPrincipal.style.display = "none";
+    vistaDatos_Personales.style.display = "block";
+    divBotonVolver.style.display = "flex";
+
+
+})
+
 const botonVolver = document.getElementById("volver");
 botonVolver.addEventListener("click", () => {
     menuPrincipal.style.display = "flex";
     vistaMisTurnos.style.display = "none";
     vistaNuevoTurno.style.display = "none";
     divBotonVolver.style.display = "none";
+    vistaDatos_Personales.style.display ="none"
+
 })
