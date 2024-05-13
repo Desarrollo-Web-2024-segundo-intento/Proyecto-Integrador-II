404f5b902c7330a975c8f9053dc0c01357131c0a
Alejo11Lucero <86329591+Alejo11Lucero@users.noreply.github.com>
2024-05-12 21:43:39 -0300

    Se sube termino y condiciones

--- reserva-medica-online/src/app/shared/footer/footer.component.ts
+++ reserva-medica-online/src/app/shared/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PoliticasComponent } from '../../pages/politicas/politicas.component';
+import { TerminosYCondicionesComponent } from '../../pages//terminos-y-condiciones/terminos-y-condiciones.component';
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FooterComponent, RouterLink, FooterComponent, PoliticasComponent],
+  imports: [FooterComponent, RouterLink, FooterComponent, PoliticasComponent, TerminosYCondicionesComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
