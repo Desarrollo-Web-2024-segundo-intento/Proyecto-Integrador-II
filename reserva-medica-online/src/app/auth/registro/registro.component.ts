9bb113850a7e26ff64bb81db287cbc855269af9c
Jesica Aramayo <jess.ara95@outlook.com>
2024-05-23 19:41:49 -0300

    cambios en formularios reactivos registro y contacto

--- reserva-medica-online/src/app/auth/registro/registro.component.ts
+++ reserva-medica-online/src/app/auth/registro/registro.component.ts
@@ -11,36 +11,53 @@ import { Router } from '@angular/router';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-  form:FormGroup;
+  registroError:string="";
+  registroForm=this.formBuilder.group({
+    apellido:['',[Validators.required, this.nameValidator()], []],
+    nombre:['',[Validators.required, this.nameValidator()], []],
+    telefono:['',[Validators.required, this.telefonoValidator()], []],
+    dni:['',[Validators.required, this.dniValidator()], []],
+    mutual:['',[Validators.required, this.mutualValidator()], []],
+    email:['',[Validators.required, this.emailValidator()], []],
+    password:['',[Validators.required, this.passwordValidator], []]
+  })
 
-  constructor(private formBuilder:FormBuilder, private router: Router) {
-    this.form=this.formBuilder.group(
-      {
-        apellido:['',[Validators.required, this.nameValidator()], []],
-        nombre:['',[Validators.required, this.nameValidator()], []],
-        telefono:['',[Validators.required, this.telefonoValidator()], []],
-        dni:['',[Validators.required, this.dniValidator()], []],
-        mutual:['',[Validators.required, this.mutualValidator()], []],
-        email:['',[Validators.required, this.emailValidator()], []],
-        password:['',[Validators.required, this.passwordValidator], []]
-      }
-    )
+  constructor(private formBuilder:FormBuilder, private router:Router) { }
+
+  ngOnInit(): void {
   }
 
-  ngOnInit() {
+  get apellido(){
+    return this.registroForm.controls.apellido;
+  }
+  get nombre(){
+    return this.registroForm.controls.nombre;
+  }
+  get telefono(){
+    return this.registroForm.controls.telefono;
+  }
+  get dni(){
+    return this.registroForm.controls.dni;
+  }
+  get mutual(){
+    return this.registroForm.controls.mutual;
+  }
+  get email(){
+    return this.registroForm.controls.email;
+  }
+  get password(){
+    return this.registroForm.controls.password;
   }
 
-  onEnviar(event: Event) {
-    console.log(this.form.value)
-      event.preventDefault;
-      if (this.form.valid){
-        alert ("El registro fue exitoso. Por favor, Inicie Sesión")
-        this.form.reset()
-        this.router.navigate(['/iniciarSesion'])
-      }
-      else {
-        this.form.markAllAsTouched();
-      }
+  registrar() {
+  if (this.registroForm.valid){
+    alert ("El registro fue exitoso. Por favor, Inicie Sesión")
+    this.router.navigate(['/iniciarSesion'])
+    this.registroForm.reset()
+  }
+  else {
+    this.registroForm.markAllAsTouched();
+  }
   }
 
   nameValidator(): ValidatorFn {
@@ -59,13 +76,13 @@ export class RegistroComponent implements OnInit {
     };
   }
 
-   telefonoValidator(): ValidatorFn {
-     return (control: AbstractControl): { [key: string]: any } | null => {
-       const telefono = control.value;
-       const regex = /^[0-9]{10,10}$/;
-       return regex.test(telefono)? null : { 'invalidTelefono': { value: control.value } };
-     };
-   }
+  telefonoValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const telefono = control.value;
+      const regex = /^[0-9]{10,10}$/;
+      return regex.test(telefono)? null : { 'invalidTelefono': { value: control.value } };
+    };
+  }
 
   dniValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
@@ -78,40 +95,16 @@ export class RegistroComponent implements OnInit {
   mutualValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
       const mutual = control.value;
-      //const regex = /^[A-Za-z]{3,}$/;
       const regex = /^([a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]{2,60}[\,\-\.]{0,1}[\s]{0,1}){1,3}$/;
       return regex.test(mutual)? null : { 'invalidMutual': { value: control.value } };
     };
   }
 
-  passwordValidator(control: AbstractControl): { [key: string]: any } | null {
-    const password = control.value;
-    const regex = /^(?=(?:.*\d))(?=.*[A-Z])(?=.*[a-z])(?=.*[.,*!?¿¡/#$%&])\S{8,20}$/;
-    return regex.test(password)? null : { 'passwordStrength': { value: control.value } };
+  passwordValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const password = control.value;
+      const regex = /^(?=(?:.*\d))(?=.*[A-Z])(?=.*[a-z])(?=.*[.,*!?¿¡/#$%&])\S{8,20}$/;
+      return regex.test(password)? null : { 'invalidPassword': { value: control.value } };
+    };
   }
-
-
-
-  get Apellido(){
-    return this.form.get("apellido");
-  }
-  get Nombre(){
-    return this.form.get("nombre");
-  }
-  get Telefono(){
-    return this.form.get("telefono");
-  }
-  get Dni(){
-    return this.form.get("dni");
-  }
-  get Mutual(){
-    return this.form.get("mutual");
-  }
-  get Password(){
-    return this.form.get("password");
-  }
-  get Email(){
-    return this.form.get("email");
-  }
-
 }
