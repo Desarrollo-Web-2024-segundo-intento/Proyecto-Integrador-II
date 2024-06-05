9ab7d1d24ba894978405b1fd0265d304756ddd52
Monica <moniantich@gmail.com>
2024-06-05 20:16:55 -0300

    boton agendar turno/esoecilidades

--- reserva-medica-online/src/app/pages/servicios/servicios.component.ts
+++ reserva-medica-online/src/app/pages/servicios/servicios.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EspecialMedicasComponent } from '../especial-medicas/especial-medicas.component';
-
-
+import { routes } from '../../app.routes';
+import { Router, RouterLink } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+ import { TurnosComponent } from '../turnos/turnos.component';
 
 @Component({
   selector: 'app-servicios',
@@ -10,16 +13,44 @@ import { EspecialMedicasComponent } from '../especial-medicas/especial-medicas.c
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.css'
 
+
 })
+// export class ServiciosComponent {
+// imgBajaComplejidad="assets/img/imga.png";
+// imgMedianaComplejidad="assets/img/imgb.png";
+// imgAltaComplejidad="assets/img/imgc.png";
+// imgObraSocial1="assets/img/medife.png";
+// imgObraSocial2="assets/img/osde.png";
+// imgObraSocial3="assets/img/sancor.png";
+// imgObraSocial4="assets/img/galeno.jpg";
+// imgObraSocial5="assets/img/Prevencion_Salud.png";
+// imgObraSocial6="assets/img/avalian.png";
+// imgObraSocial7="assets/img/pami.jpg";
+// imgObraSocial8="assets/img/apross.jpg";
+// imgObraSocial9="assets/img/swissmedical.png"
+// imgObraSocial10="assets/img/federadasalud.jpg";
+// imgObraSocial11="assets/img/sipssa.jpg";
+
+
+//  })
+
+
+
+
+
 
 export class ServiciosComponent implements OnInit {
+  router: any;
+
 
   constructor() { }
+
 
   ngOnInit() {
     console.log('Se ha iniciado el componente de servicios.');
     this.fetchData();
   }
+
 
   async fetchData() {
     try {
@@ -33,13 +64,18 @@ export class ServiciosComponent implements OnInit {
       const data = await response.json();
       console.log('Datos recibidos:', data);
 
+
           for (let i = 0; i < data.length; i++) {
+
 
         const newDiv = document.createElement('div'); // este constante genera el div contenedor de la especialidad
         newDiv.id = 'div' + i;
         newDiv.style.width = '22%';
         newDiv.style.height = '400px';
         newDiv.style.backgroundColor = 'white';
+        newDiv.style.backgroundImage = 'url("assets/img/clinico.png")'; // Cambia esta ruta a la de tu imagen
+        newDiv.style.backgroundSize = 'cover';
+        newDiv.style.backgroundPosition = 'center';
         newDiv.style.display = 'flex';
         newDiv.style.flexDirection = 'column';
         newDiv.style.justifyContent = 'center';
@@ -54,35 +90,46 @@ export class ServiciosComponent implements OnInit {
         } else {
             console.error('No se encontró el elemento con id "app"');
         }
+        const div_padre = document.getElementById('div'+i); // Asegúrate de que este sea el ID correcto
+        
+        
         const div_contenedor = document.createElement('div'); // Crea el div contenedor de div_titulo y div_carrito
         div_contenedor.style.width = '100%';
-        div_contenedor.style.backgroundColor = 'yellow';
+        div_contenedor.style.backgroundColor = 'transparent';
         div_contenedor.style.display = 'flex';
         div_contenedor.style.flexDirection = 'row';
         div_contenedor.style.justifyContent = 'center';
         div_contenedor.style.alignItems = 'center';
-        newDiv.appendChild(div_contenedor);
+        // newDiv.appendChild(div_contenedor);
+        
+        if (div_padre) {
+          div_padre.appendChild(div_contenedor);
+        } else {
+          console.error('No se encontró el elemento con id "app"');
+        }
 
         const div_titulo = document.createElement('div'); // este constante genera el div contenedor de la especialidad
         div_titulo.id = 'div_titulo';
         div_titulo.innerText = data[i].especialidad;
         div_titulo.style.width = '55%';
         div_titulo.style.height = '50px';
-        div_titulo.style.backgroundColor = 'green';
+        div_titulo.style.backgroundColor = 'transparent'; 
         div_titulo.style.display = 'flex';
         div_titulo.style.flexDirection = 'column';
         div_titulo.style.justifyContent = 'center';
         div_titulo.style.alignItems = 'center';
         div_titulo.style.border = '2px solid black';
         div_titulo.style.margin = '10px'; // Margen para separación entre divs
-        div_contenedor.appendChild(div_titulo);
+        // div_contenedor.appendChild(div_titulo);
 
-        const div_padre = document.getElementById('div'+i); // Asegúrate de que este sea el ID correcto
-        if (div_padre) {
-            div_padre.appendChild(div_titulo);
+
+        //  const div_padre = document.getElementById('div'+i); // Asegúrate de que este sea el ID correcto
+        if (div_contenedor) {
+            div_contenedor.appendChild(div_titulo);
         } else {
             console.error('No se encontró el elemento con id "app"');
         }
+
 
         const div_carrito = document.createElement('div'); // este constante genera el div contenedor de la especialidad
         div_carrito.id = 'div_carrito';
@@ -97,32 +144,23 @@ export class ServiciosComponent implements OnInit {
         div_carrito.style.border = '1px solid black';
         div_carrito.style.margin = '10px'; // Margen para separación entre divs
 
-            // Añadir div_titulo y div_carrito a div_contenedor
-            div_contenedor.appendChild(div_titulo);
+        if (div_contenedor) {
             div_contenedor.appendChild(div_carrito);
+        } else {
+            console.error('No se encontró el elemento con id "app"');
+        }
 
 
-            // Crear y configurar la imagen
+
+
+            // // Crear y configurar la imagen
             const img = document.createElement('img');
             img.src = 'assets/img/carrito.png'; // Cambia esta ruta a la de tu imagen
             img.alt = 'Carrito';
             img.style.width = '50px'; // Ajusta el tamaño según sea necesario
             img.style.height = '50px'; // Ajusta el tamaño según sea necesario
+            div_carrito.appendChild(img)
 
-            // Añadir la imagen al div_carrito
-            div_carrito.appendChild(img);
-
-
-
-
-
-
-
-
-
-
-        // Añadir div_contenedor a newDiv
-          // newDiv.appendChild(div_contenedor);
 
         const div_descripcion = document.createElement('div'); // este constante genera el div contenedor de la especialidad
         div_descripcion.id = 'div_descripcion';
@@ -130,7 +168,7 @@ export class ServiciosComponent implements OnInit {
         div_descripcion.style.width = '98%';
         div_descripcion.style.height = '450px';
         div_descripcion.style.font='12px';
-        div_descripcion.style.backgroundColor = 'gray';
+        div_descripcion.style.backgroundColor = 'transparent';
         div_descripcion.style.display = 'flex';
         div_descripcion.style.flexDirection = 'column';
         div_descripcion.style.justifyContent = 'center';
@@ -139,26 +177,29 @@ export class ServiciosComponent implements OnInit {
         // div_titulo.style.float = 'left';
         div_descripcion.style.margin = '10px'; // Margen para separación entre divs
 
+
         if (div_padre) {
             div_padre.appendChild(div_descripcion);
         } else {
             console.error('No se encontró el elemento con id "app"');
         }
 
+
         const div_boton_turno = document.createElement('div'); // este constante genera el div contenedor de la especialidad
-        div_boton_turno.id = 'div_descripcion';
-        div_boton_turno.innerText = 'Agendar Turno';
-        div_boton_turno.style.width = '90%';
-        div_boton_turno.style.height = '50px';
-        div_boton_turno.style.font='12px';
-        div_boton_turno.style.backgroundColor = 'orange';
-        div_boton_turno.style.display = 'flex';
-        div_boton_turno.style.flexDirection = 'column';
-        div_boton_turno.style.justifyContent = 'center';
-        div_boton_turno.style.alignItems = 'center';
-        div_boton_turno.style.border = '2px solid black';
-        // div_titulo.style.float = 'left';
-        div_boton_turno.style.margin = '10px'; // Margen para separación entre divs
+        div_boton_turno.id = 'div_boton_turno';
+        // // div_boton_turno.innerText = 'Agendar Turno';
+        // // // div_boton_turno.style.width = '90%';
+        // // // div_boton_turno.style.height = '50px';
+        // // div_boton_turno.style.font='12px';
+        // // div_boton_turno.style.backgroundColor = 'transparent';
+        // // div_boton_turno.style.display = 'flex';
+        // // div_boton_turno.style.flexDirection = 'column';
+        // // div_boton_turno.style.justifyContent = 'center';
+        // // div_boton_turno.style.alignItems = 'center';
+        // // div_boton_turno.style.border = '2px solid black';
+        // // // div_titulo.style.float = 'left';
+        // // div_boton_turno.style.margin = '10px'; // Margen para separación entre divs
+
 
         if (div_padre) {
             div_padre.appendChild(div_boton_turno);
@@ -166,18 +207,24 @@ export class ServiciosComponent implements OnInit {
             console.error('No se encontró el elemento con id "app"');
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        const boton_turno = document.createElement('button');
+        boton_turno.innerText = 'Agendar Turno';
+        boton_turno.style.width = '90%';
+        boton_turno.style.height = '50px';
+        boton_turno.style.font='12px';
+        boton_turno.style.backgroundColor = 'transparent';
+        boton_turno.style.display = 'flex';
+        boton_turno.style.flexDirection = 'column';
+        boton_turno.style.justifyContent = 'center';
+        boton_turno.style.alignItems = 'center';
+        boton_turno.style.border = '2px solid black';
+        //titulo.style.float = 'left';
+        boton_turno.style.margin = '10px';
+        boton_turno.addEventListener('click', () => {
+          this.router.navigateByUrl(['/turnos']);
+        });
+        div_boton_turno.appendChild(boton_turno);
+      
 
 
       }
@@ -186,12 +233,9 @@ export class ServiciosComponent implements OnInit {
     }
   }
 
+
   onServiciosClick(event: Event) {
     event.preventDefault();
     this.fetchData();
   }
-
 }
-
-
-
